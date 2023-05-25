0a1940651eef8cba495b0dfa3438fda4c7ea10bc
MSI CREATOR M16 <MSI CREATOR M16@MSI>
2023-05-25 20:05:29 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HelloWordController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        public IActionResult Index(string name, int age = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["Age"] = age;
+            return View();
+        }
+     //   public string Index()
+     //   {
+     //       return "Hello My Name Is Lam ^^";
+     //   }
+      //  public string Welcome()
+      //  {
+      //      return "Hello My Name Is Lam";
+      //  }
+      public string Welcome(string name , int age=1 )
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, age: {age}");
+        }
+    }
+}
